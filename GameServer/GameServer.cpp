b44b14fbb48c9e820e25bf9c80c8489390b37d41
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2022-10-21 22:30:24 +0900

    인프런 쓰래드개론

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,7 +3,7 @@
 #include "CorePch.h"
 
 #include <thread>
-
+#include <mutex>
 void HelloThread()
 {
 	cout << "헬로 쓰레드" << endl;
@@ -16,25 +16,17 @@ void HelloThread_2(int32 num)
 }
 int main()
 {
-	// 쓰레드 객체
 	vector<thread> v;
 
-	v.resize(10);
+	for (int32 i = 0; i < 10; ++i) {
+		v.push_back(thread(HelloThread_2, i));
+	}
 
-	thread t;
-	auto id1 = t.get_id();
-	
-	t = thread(HelloThread);
+	for (int32 i = 0; i < 10; ++i) {
+		if (v[i].joinable())
+			v[i].join();
+	}
 
-	cout << "Hello Main" << endl;
-	// 이 다섯개만 알고있자.
-	int32 cnt = t.hardware_concurrency(); // cpu 코어의 개수?
+	cout << "헬로 메인" << endl;
 	
-	auto id2 = t.get_id();		// 쓰레드마다 갖고 있는 id 1 2 3 4 로 늘어나지 않고 들쑥날쑥 좆같은 숫자이지만 구분은 됨
-	
-	//t.detach();		// thread객체에서 실제 쓰레드를 분리 (연결고리를 끊어버림) 앵간해서는 거의 안씀 알고만 있자.
-	
-	if(t.joinable())	 
-		t.join();
-
 }
