f217d76e75758f71322039a3421ea90369e0ad62
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2023-06-12 18:16:53 +0900

    CPU파이프라인
    섹션 1. 멀티스레드 프로그래밍
    CPU 파이프라인

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,38 +8,64 @@
 #include <windows.h>
 #include <future>
 
-// [][][][][][]		[][][][][][]	[][][][][][]	[][][][][][]	[][][][][][][]
+// 문제의 원인
+// 가시성, 재배치
 
+// 가시성
+// cpu가 어떤 값을 쓰거나 읽을 때 곧이 곧대로 ram까지 100%확률로 가지 않음
+// cpu에서 각 코어마다 자기만의 캐시를 갖고 있슴
+// 
+// 코드재배치
+// 우리는 cpu와 컴파일러에게 속고 있었다.
+// 우리가 작업한 코드를 컴파일러가 그대로 번역하지 않을 수 있다.
+// 컴파일러가 검수할 때 순서를 바꾸면 속도가 더 빠르지 않을까라고 판단하면 코드를 재배치함.
+// 컴파일러가 안하더라고 cpu가 바꿀수있슴 ㅡㅡ
+// 
+//
+int32 x = 0;
+int32 y = 0;
+int32 r1 = 0;
+int32 r2 = 0;
 
-int32 buffer[10000][10000];
+volatile bool ready;
 
+void Thread_1()
+{
+	while (!ready);
+	y = 1;	// Store y
+	r1 = x; // Load x
+	
+}
+
+void Thread_2()
+{
+	while (!ready);
+
+	x = 1;	// Store x
+	r2 = y; // Load y
+}
 int main()
 {
-	memset(buffer, 0, sizeof(buffer));
+	int32 count = 0;
 
+	while (true)
 	{
-		uint64 start = GetTickCount64();
-		int64 sum = 0;
+		ready = false;
 
-		for (int32 i = 0; i < 10000; ++i)
-			for (int32 j = 0; j < 10000; ++j)
-				sum += buffer[i][j];
-		uint64 end = GetTickCount64();
-		cout << "Elapsed Tick " << (end - start) << endl;
+		count++;
 
+		x = y = r1 = r2 = 0;
 
+		thread t1(Thread_1);
+		thread t2(Thread_2);
+
+		ready = true;
+
+		t1.join();
+		t2.join();
+
+		if (r1 == 0 && r2 == 0)
+			break;
 	}
-
-	{
-		uint64 start = GetTickCount64();
-		int64 sum = 0;
-
-		for (int32 i = 0; i < 10000; ++i)
-			for (int32 j = 0; j < 10000; ++j)
-				sum += buffer[j][i];
-		uint64 end = GetTickCount64();
-		cout << "Elapsed Tick " << (end - start) << endl;
-
-
-	}
+	cout << count << " 번만에 빠져나옴" << endl;
 }
