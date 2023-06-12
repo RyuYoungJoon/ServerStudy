e4df89568040958e98ba13566b4d66d37b8d3623
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2023-06-12 17:50:45 +0900

    Cache
    섹션 1. 멀티쓰레드 프로그래밍
    캐시

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,93 +8,38 @@
 #include <windows.h>
 #include <future>
 
-
-int64 result;
-
-int64 Calculate()
-{
-	int64 sum = 0;
-
-	for (int32 i = 0; i < 100'000; ++i)
-		sum += i;
-
-	result = sum;
-
-	return sum;
-}
-
-void PromiseWorker(std::promise<string>&& promise)
-{
-	promise.set_value("Secret Message");
-}
-
-void TaskWorker(std::packaged_task<int64(void)>&& task)
-{
-	task();
+// [][][][][][]		[][][][][][]	[][][][][][]	[][][][][][]	[][][][][][][]
 
 
-}
+int32 buffer[10000][10000];
+
 int main()
 {
-	// 동기(synchronous) 실행
-	//Calculate();
+	memset(buffer, 0, sizeof(buffer));
 
-
-	//thread t1(Calculate);
-
-	//std::future
 	{
-		// 1) deferred -> lazy evaluation 지연해서 실행하세요
-		// 2) async -> 별도의 쓰레드를 만들어서 실행하세요
-		// 3) deferred | async -> 둘 중 알아서 골라주세요.
-		
-		// 언젠간 미래에 결과물을 가져다줄거야!
-		std::future<int64> future = std::async(std::launch::async, Calculate);
+		uint64 start = GetTickCount64();
+		int64 sum = 0;
 
-		int64 sum = future.get();		// 결과물이 이제서야 필요하다.
+		for (int32 i = 0; i < 10000; ++i)
+			for (int32 j = 0; j < 10000; ++j)
+				sum += buffer[i][j];
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick " << (end - start) << endl;
+
 
 	}
 
-	// std::promise
 	{
-		// 미래(std::future)에 결과물을 반환해줄꺼라 약속(std::promise) (계약서?)
-		std::promise<string> promise;
-		std::future<string> future = promise.get_future();
+		uint64 start = GetTickCount64();
+		int64 sum = 0;
 
-		thread t(PromiseWorker, std::move(promise));
+		for (int32 i = 0; i < 10000; ++i)
+			for (int32 j = 0; j < 10000; ++j)
+				sum += buffer[j][i];
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick " << (end - start) << endl;
 
-
-		string message = future.get();
-
-		cout << message << endl;
-		t.join();
-	}
-
-	// std::packaged_task
-	{
-		std::packaged_task<int64(void)> task(Calculate);
-		std::future<int64> future = task.get_future();
-
-		std::thread t(TaskWorker, std::move(task));
-
-		int64 sum = future.get();
-		cout << sum << endl;
-
-		t.join();
 
 	}
-
-	// 결론
-	// mutex, condition_variable까지 가지 않고 단순한 애들을 처리 할 수 있는
-	// 1회성으로 한번만 일어나는 이벤트에 유용함
-	// 특히나, 한 번 발생하는 이벤트에 유용하다!
-	// 닭잡는데 소잡는 칼을 쓸 필요 없다~
-
-	// 1) async
-	// 원하는 함수를 비동기적으로 실행
-	// 2) promise
-	// 결과물을 promise를 통해 future로 받아줌
-	// 3) packaged_task
-	// 원하는 함수의 실행 결과를 packaged_task를 통해 future로 받아줌
-
 }
