785d991410bbdd284a87f535657ffb4fd9a176d6
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2023-06-12 19:01:21 +0900

    Memory Model(1)
    섹션 1. MemoryModel(1)
    atomic 연산에 대해서 공부.
    
    atomic store, load, exchange ,cas
    동일 수정 순서를 보장하기 위해
    몇가지의 단계가 있는 연산을
    한번에 원자적인 연산을 도와줌

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,64 +8,71 @@
 #include <windows.h>
 #include <future>
 
-// 문제의 원인
-// 가시성, 재배치
+atomic<bool> flag;
 
-// 가시성
-// cpu가 어떤 값을 쓰거나 읽을 때 곧이 곧대로 ram까지 100%확률로 가지 않음
-// cpu에서 각 코어마다 자기만의 캐시를 갖고 있슴
-// 
-// 코드재배치
-// 우리는 cpu와 컴파일러에게 속고 있었다.
-// 우리가 작업한 코드를 컴파일러가 그대로 번역하지 않을 수 있다.
-// 컴파일러가 검수할 때 순서를 바꾸면 속도가 더 빠르지 않을까라고 판단하면 코드를 재배치함.
-// 컴파일러가 안하더라고 cpu가 바꿀수있슴 ㅡㅡ
-// 
-//
-int32 x = 0;
-int32 y = 0;
-int32 r1 = 0;
-int32 r2 = 0;
-
-volatile bool ready;
-
-void Thread_1()
-{
-	while (!ready);
-	y = 1;	// Store y
-	r1 = x; // Load x
-	
-}
-
-void Thread_2()
-{
-	while (!ready);
-
-	x = 1;	// Store x
-	r2 = y; // Load y
-}
 int main()
 {
-	int32 count = 0;
+	flag.store(false);
 
-	while (true)
+	// 왜 락이 없냐? 락이 없어도 원자적으로 처리 할 수 있기때문에 true
+	// false면 원자적으로 처리하려면 락이 피료여하다.
+	
+	//flag.is_lock_free();
+	
+
+	//flag = true;
+	flag.store(true,memory_order::memory_order_seq_cst);
+
+	// bool val = flag;
+	bool val = flag.load(memory_order::memory_order_seq_cst);
+
+	// 이전 flag 값을 prev에 넣고, flag 값을 수정
 	{
-		ready = false;
 
-		count++;
+		// 이건 좀 문제가 있다.
+		//bool prev = flag;
+		//flag = true;
+		// flag값을 prev에 넣었다고 생각하겠지만. 순간 flag값을 다른 스레드가 접근했다고 생각하면
+		// 이 값은 잘못된 값이다.
+		// 결국 저 2줄의 코드가 원자적으로 한번에 이뤄져야함.
+		// 읽고 쓰는것을 한번에 해주는 놈이 있다.
+		// exchange
+		// 넣기 직전에 값을 저장해주는것
+		bool prev = flag.exchange(true);
 
-		x = y = r1 = r2 = 0;
-
-		thread t1(Thread_1);
-		thread t2(Thread_2);
-
-		ready = true;
-
-		t1.join();
-		t2.join();
-
-		if (r1 == 0 && r2 == 0)
-			break;
 	}
-	cout << count << " 번만에 빠져나옴" << endl;
+
+	// CAS (Compare-And-Swap) 조건부 수정
+	{
+		// 만약 내가 기대하는 값이 expected라면 desire로 바꿔라
+		bool expected = false;
+		bool desired = true;
+		flag.compare_exchange_strong(expected, desired);
+
+		// 위에 코드는 이 뜻이다.
+		// 이 코드를 원자적으로 해주는 cas
+		/*if (flag == expected)
+		{
+			flag = desired;
+			flag = true;
+		}
+		else
+		{
+			expected = flag;
+			return false;
+		}*/
+
+		// strong과 weak는 거의 똑같음
+		//while (true) {
+		//	bool exptected = false;
+		//	bool desired = true;
+		//	flag.compare_exchange_weak(expected, desired);
+		//}
+		// 하나 차이가 있다.
+		// Spurious Failure (가짜 실패)
+		// 동작은 똑같지만 만약 다른 쓰레드의 interruption을 받아서 중간에 실패한다면
+		// false를 반환 할 수도 있다.
+		// 근데 strong은 성공 할 때 까지 반복해서 true를 받아낸다.
+		// weak는 while과 같이 쓴다.
+	}
 }
