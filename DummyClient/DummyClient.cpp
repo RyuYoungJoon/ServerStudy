3629409c72addf1cd535c45db051e5763b632c15
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2023-08-02 15:58:08 +0900

    TCP 서버 실습
    send recv 응용버퍼에 대해

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -8,16 +8,10 @@
 
 int main()
 {
-	// 윈속 초기화 (ws2_32 라이브러리 초기화)
-	// 관련 정보가 wsaData에 채워짐
 	WSADATA wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
 		return 0;
 
-	// af : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
-	// type : TCP(SOCK_STREAM) VS UDP(SOCK_DGRAM)
-	// protocol : 0
-	// return : decriptor
 	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
@@ -26,18 +20,11 @@ int main()
 		return 0;
 	}
 
-	// 연결할 목적지는? (ip주소 + port) -> xx아파트 xx호
 	SOCKADDR_IN serverAddr; // IPv4
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1"); << 쓰지말라함
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 	serverAddr.sin_port = htons(7777); // 80 : HTTP
-	// host to network short
-	// Little-Endian vs Big-Endian
-	// ex) 12345678 4바이트 정수
-	// [0x78][0x56][0x34][0x12] Little
-	// [0x12][0x34][0x56][0x78] Big = network
 
 	if(::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 	{
@@ -53,8 +40,37 @@ int main()
 	while (true)
 	{
 		// TODO
+		char sendBuffer[100] = "HelloWorld";
 
-		this_thread::sleep_for(10ms);
+		for (int32 i = 0; i < 12; ++i)
+		{
+			int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+			if (resultCode == SOCKET_ERROR)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Connect ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		}
+
+		cout << "Send Data! Len : " << sizeof(sendBuffer) << endl;
+
+
+		/*char recvBuffer[1000];
+
+		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+		if (recvLen <= 0)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Connect ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		cout << "Recv Data! Data : " << recvBuffer << endl;
+		cout << "Recv Data! Len : " << recvLen << endl;*/
+
+		this_thread::sleep_for(1s);
 	}
 
 
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -18,16 +18,10 @@
 
 int main()
 {
-	// 윈속 초기화 (ws2_32 라이브러리 초기화)
-	// 관련 정보가 wsaData에 채워짐
 	WSADATA wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
 		return 0;
 
-	// af : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
-	// type : TCP(SOCK_STREAM) VS UDP(SOCK_DGRAM)
-	// protocol : 0
-	// return : decriptor
 	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (listenSocket == INVALID_SOCKET)
 	{
@@ -36,7 +30,6 @@ int main()
 		return 0;
 	}
 
-	// 나의 주소는? (ip주소 + port) -> xx아파트 xx호
 	SOCKADDR_IN serverAddr; // IPv4
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
@@ -81,6 +74,33 @@ int main()
 		cout << "Client Connected! IP : " << ipAddress << endl;
 
 		// TODO
+		while (true)
+		{
+			char recvBuffer[1000];
+
+			this_thread::sleep_for(1s);
+
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen <= 0)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "Connect ErrorCode : " << errCode << endl;
+				return 0;
+			}
+
+			cout << "Recv Data! Data : " << recvBuffer << endl;
+			cout << "Recv Data! Len : " << recvLen << endl;
+
+			/*int32 resultCode = ::send(clientSocket, recvBuffer, recvLen, 0);
+			if (resultCode == SOCKET_ERROR)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "Connect ErrorCode : " << errCode << endl;
+				return 0;
+			}*/
+
+		}
+
 	}
 
 	//---------------------------------
