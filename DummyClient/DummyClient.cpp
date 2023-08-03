ae287b4334098fff2880c2064528499e63f36d82
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2023-08-03 14:15:32 +0900

    논블로킹 소켓
    ioctlsocket
    WSAEWOULDBLOC  = 원래 블록했어야했는데 너가 논블로킹으로 하라며라는 오류

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -18,14 +18,81 @@ int main()
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
 		return 0;
 
-	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
 		HandleError("Socket");
 		return 0;
 	}
 
+	u_long on = 1;
+	if (::ioctlsocket(clientSocket, FIONBIO, &on) == INVALID_SOCKET)
+		return 0;
 
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(8888);
+
+	// Connect
+	while (true)
+	{
+		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			// 이미 연결된 상태라면 break
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
+			// ERROR
+			break;
+		}
+	}
+
+	cout << "Connected To Server" << endl;
+
+	char sendBuffer[100] = "HelloWorld!";
+	// Send
+	while (true)
+	{
+		if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0))
+		{
+			// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			// ERROR
+			break;
+		}
+
+		cout << "Send Data! Len : " << sizeof(sendBuffer) << endl;
+
+
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+				// ERROR
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				// 연결 끊킴
+				break;
+			}
+
+			cout << "Recv Data! Len : " << recvLen << endl;
+			break;
+		}
+
+		this_thread::sleep_for(1s);
+	}
 	//-----------------------------
 	
 	// 소켓 리소스 반환
